d7d404fd39e72dabcdf4e7ea782b073a67d18c3a
Gennadiy <gonzy@yandex.ru>
2020-05-05 15:36:54 +0400

    Сделал интерфейс программы. Начата работа с логикой. Начата работа с MVVM.

---
+++ MailSender.lib/Data/TestData.cs
@@ -0,0 +1,22 @@
+﻿using MailSender.lib.Entities;
+using System.Collections.Generic;
+
+namespace MailSender.lib.Data
+{
+    public static class TestData
+    {
+        public static List<Server> servers { get; } = new List<Server>
+        {
+            new Server {Name = "Яндекс", Adress = "smtp.yandex.ru", LogIn = "login yandex", Password = "passwordYandex", Port = 587},
+            new Server {Name = "Гугл", Adress = "smtp.gmail.com", LogIn = "login gmail", Password = "passwordGmail", Port = 25},
+            new Server {Name = "Mail.ru", Adress = "smtp.mail.ru", LogIn = "login mail", Password = "passwordMail", Port = 587}
+        };
+
+        public static List<Sender> senders { get; } = new List<Sender>
+        {
+            new Sender {Name = "Иванов", Adress = "ivanov@server.ru"},
+            new Sender {Name = "Петров", Adress = "petrov@server.ru"},
+            new Sender {Name = "Сидоров", Adress = "sidorov@server.ru"}
+        };
+    }
+}
---
+++ MailSender.lib/Entities/Server.cs
@@ -0,0 +1,13 @@
+﻿namespace MailSender.lib.Entities
+{
+    /// <summary>Почтовый сервер </summary>
+    public class Server
+    {
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public int Port { get; set; }
+        public bool UseSSL { get; set; } = true;
+        public string LogIn { get; set; }
+        public string Password { get; set; }
+    }
+}
