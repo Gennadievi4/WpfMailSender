8db17e7b189155d3d1b56ee6bbc4f2a1bd266243
Gennadiy <gonzy@yandex.ru>
2020-05-11 16:10:01 +0400

    реализовал интерфейс

---
+++ MailSender.lib/Services/Interfaces/IRecipientStore.cs
@@ -0,0 +1,15 @@
+﻿using MailSender.lib.Entities;
+using System.Collections.Generic;
+
+namespace MailSender.lib.Services.Interfaces
+{
+    public interface IRecipientStore
+    {
+        IEnumerable<Recipient> GetAll();
+        Recipient GetById(int Id);
+        int Create(Recipient recipient);
+        void Edit(int Id, Recipient recipient);
+        Recipient Remove(int Id);
+        void SaveChanges();
+    }
+}
---
+++ MailSender.lib/Services/RecipientsManager.cs
@@ -0,0 +1,27 @@
+﻿using MailSender.lib.Entities;
+using MailSender.lib.Services.Interfaces;
+using System.Collections.Generic;
+
+namespace MailSender.lib.Services
+{
+    public class RecipientsManager : IRecipientManager
+    {
+        private IRecipientStore _Store;
+
+        public RecipientsManager(IRecipientStore Store) { _Store = Store; }
+        public IEnumerable<Recipient> GetAll() => _Store.GetAll();
+        public void Add(Recipient recipient)
+        {
+
+        }
+        public void Edit(Recipient recipient)
+        {
+            _Store.Edit(recipient.ID, recipient);
+        }
+
+        public void SaveChanges()
+        {
+            _Store.SaveChanges();
+        }
+    }
+}
