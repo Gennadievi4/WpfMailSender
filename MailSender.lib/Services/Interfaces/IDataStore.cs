ab508691d5c98b1f5395b572d995ba13c0abeb16
Gennadiy <gonzy@yandex.ru>
2020-05-11 23:08:27 +0400

    Интерфейсы сервисов хранения данных

---
+++ MailSender.lib/Services/Interfaces/IDataStore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender.lib.Services.Interfaces
+{
+    public interface IDataStore<T>
+    {
+        IEnumerable<T> GetAll();
+        void Edit(int Id, T recipient);
+        void SaveChanges();
+        T GetById(int Id);
+        int Create(T T);
+        T Remove(int Id);
+    }
+}
